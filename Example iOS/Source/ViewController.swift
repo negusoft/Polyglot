371ab9bd1940a433e79571bdfae189d0c1cee34c
blurkidi <blurkidi@negusoft.com>
2015-09-11 16:04:12 +0200

    Add empty iOS example app

---
+++ Example iOS/Source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Polyglot Example iOS
+//
+//  Created by Borja Lopez Urkidi on 11/09/15.
+//  Copyright (c) 2015 NEGU Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Example iOS/Tests/Polyglot_Example_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Polyglot_Example_iOSTests.swift
+//  Polyglot Example iOSTests
+//
+//  Created by Borja Lopez Urkidi on 11/09/15.
+//  Copyright (c) 2015 NEGU Soft. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Polyglot_Example_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
