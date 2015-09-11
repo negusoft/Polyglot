d662fe6a353a3610f7d6d92757106ff353369c98
blurkidi <blurkidi@negusoft.com>
2015-09-11 17:07:42 +0200

    Add UILabel extension

--- Polyglot/Source/UILabel+Polyglot.swift
+++ Polyglot/Source/UILabel+Polyglot.swift
@@ -21,18 +21,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+@IBDesignable
+public extension UILabel {
+    
+    @IBInspectable
+    var textStringKey: String {
+        get { return "" }
+        set {
+            self.text = NSLocalizedString(newValue, comment:newValue)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
