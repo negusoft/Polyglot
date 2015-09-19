9aa9f1848fd10afd0baf619f405837f03f02a708
blurkidi <blurkidi@negusoft.com>
2015-09-19 11:26:24 +0200

    Add basic OSX example

---
+++ Example OSX/Source/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Example OSX
+//
+//  Created by borja lopez urkidi on 19/09/15.
+//  Copyright © 2015 NEGU Soft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example OSX/Source/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example OSX
+//
+//  Created by borja lopez urkidi on 19/09/15.
+//  Copyright © 2015 NEGU Soft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
